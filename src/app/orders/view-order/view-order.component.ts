15cb2b12617ba48c59a6889608026e08ddc34740
Alisson Gomes <alisson.php@gmail.com>
2017-11-23 12:28:30 -0300

    orders

--- src/app/orders/view-order/view-order.component.ts
+++ src/app/orders/view-order/view-order.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { OrdersService } from 'app/orders/orders.service';
 
 @Component({
   selector: 'app-view-order',
   templateUrl: './view-order.component.html',
-  styleUrls: ['./view-order.component.css']
+  styleUrls: ['./view-order.component.css'],
+  providers: [OrdersService]
 })
 export class ViewOrderComponent implements OnInit {
 
-  constructor() { }
+  orderId: number
+  order: any
+  errorMsg: any
+
+  constructor(private orderService: OrdersService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(
+      (params: Params) => this.orderId = params['id']
+    )
+
+    this.show()
+  }
+
+  show() {
+    this.orderService.show(this.orderId).subscribe(
+      res => this.order = res,
+      error => this.errorMsg = error
+    )
   }
 
 }
