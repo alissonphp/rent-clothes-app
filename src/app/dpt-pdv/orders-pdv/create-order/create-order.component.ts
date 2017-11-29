db50695bb3df6c46b735560550d52c8667aa7989
Alisson Gomes <alisson.php@gmail.com>
2017-11-29 19:42:58 -0300

    api integrations in order compostion

--- src/app/dpt-pdv/orders-pdv/create-order/create-order.component.ts
+++ src/app/dpt-pdv/orders-pdv/create-order/create-order.component.ts
@@ -15,10 +15,12 @@ export class CreateOrderComponent implements OnInit {
   private searchClient
   private searchItem
   private client
-  private itens: any = []
-  private resClients: any = []
-  private resItens: any = []
+  private itens: any
+  private resClients: any
+  private resItens: any
   private errorMsg
+  private loadClients = false
+  private loadItens = false
 
   constructor(private clientService: ClientsService, private itemService: ItemsService, private orderService: OrdersService) { }
 
@@ -26,14 +28,22 @@ export class CreateOrderComponent implements OnInit {
   }
 
   getSearchClient() {
+    this.loadClients = true
     this.clientService.search(this.searchClient).subscribe(
-      res => this.resClients = res,
+      res => {
+        this.resClients = res
+        this.loadClients = false
+      },
       error => this.errorMsg = error
     )
   }
   getSearchItem() {
+    this.loadItens = true
     this.itemService.search(this.searchItem).subscribe(
-      res => this.resItens = res,
+      res => {
+        this.resItens = res
+        this.loadItens = false
+      },
       error => this.errorMsg = error
     )
   }
@@ -42,5 +52,4 @@ export class CreateOrderComponent implements OnInit {
   }
   addItem() {}
   rmItem() {}
-
 }
