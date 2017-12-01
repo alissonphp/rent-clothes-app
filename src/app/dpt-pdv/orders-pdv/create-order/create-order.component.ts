274a89d9b44635f6ed4cc7353d7d4c7d60d1b16c
Alisson Gomes <alisson.php@gmail.com>
2017-12-01 00:31:31 -0300

    install momentjs and finish create order interactions

--- src/app/dpt-pdv/orders-pdv/create-order/create-order.component.ts
+++ src/app/dpt-pdv/orders-pdv/create-order/create-order.component.ts
@@ -4,6 +4,9 @@ import { ItemsService } from './../../../items/items.service';
 import { OrdersService } from './../../../orders/orders.service';
 import { ClientsService } from './../../../clients/clients.service';
 
+declare var moment: any
+declare var $: any
+
 @Component({
   selector: 'app-create-order',
   templateUrl: './create-order.component.html',
@@ -15,16 +18,24 @@ export class CreateOrderComponent implements OnInit {
   private searchClient
   private searchItem
   private client
-  private itens: any
+  private itens: any = []
   private resClients: any
   private resItens: any
   private errorMsg
+  private discount: any = 0.00
+  private out
+  private return
+  private days
+  private subtotal
+  private total
   private loadClients = false
   private loadItens = false
+  today = moment().format('YYYY-MM-DD')
 
   constructor(private clientService: ClientsService, private itemService: ItemsService, private orderService: OrdersService) { }
 
   ngOnInit() {
+
   }
 
   getSearchClient() {
@@ -36,6 +47,9 @@ export class CreateOrderComponent implements OnInit {
       },
       error => this.errorMsg = error
     )
+  }
+  calcDiff() {
+    this.days = moment(this.return).diff(this.out, 'days') + 1
   }
   getSearchItem() {
     this.loadItens = true
@@ -49,7 +63,45 @@ export class CreateOrderComponent implements OnInit {
   }
   setClient(client) {
     this.client = client
+    this.resClients = ''
+    this.searchClient = client.name
+    this.popMsg('info', 'Cliente selecionado "' + client.name + '" para a OL!', 'ti-check-box')
   }
-  addItem() {}
-  rmItem() {}
+  addOrderItem(item) {
+    item.days = this.days
+    item.subtotal = item.price * item.days
+    this.itens.push(item)
+    this.setSubtotalOrder()
+    this.popMsg('info', 'Item #' + item.code + ' adicionado à OL!', 'ti-check-box')
+  }
+  setSubtotalOrder() {
+    this.subtotal = this.itens.reduce(
+      (sub, item) => sub + item.subtotal, 0
+    )
+    this.setTotal()
+  }
+  setTotal() {
+    this.total = this.subtotal - this.discount
+  }
+  rmItem(item) {
+    const index: number = this.itens.indexOf(item)
+    this.popMsg('info', 'Item excluído!', 'ti-info-alt')
+    this.itens.splice(index, 1)
+    this.setSubtotalOrder()
+  }
+
+  popMsg(type, data, icon) {
+    $.notify({
+      icon: icon,
+      message: '<span class="text-center">' + data + '</span>'
+    }, {
+        type: type,
+        timer: 1500,
+        placement: {
+            from: 'top',
+            align: 'center'
+        }
+    });
+  }
+
 }
